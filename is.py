f2aa6a2b52cb2cccac2240454c198fe1e18dc4b0
Virgílio César <enjels@outlook.com>
2024-04-21 11:22:33 -0300

    explicação bsimples de IS e IS NOT

---
+++ is.py
@@ -0,0 +1,9 @@
+saldo = 1000
+limite = 500
+
+# "is" é usado para verifica se ocupão a mesma região de memória retorno sera false
+#verifica se le é
+print(saldo is limite)
+# "is not" é usado para verifica se realmente não ocupão a mesma região de memória  retorno sera true
+#verifica se le não é
+print(saldo is not limite)
