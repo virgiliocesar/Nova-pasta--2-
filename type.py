4e1e421ab1e31e0cc3c118a003bf64fd30607e2a
Virgílio César <enjels@outlook.com>
2024-04-21 11:13:22 -0300

    first commit

---
+++ type.py
@@ -0,0 +1,22 @@
+print (type (11 * 10 * 1000))
+print (type (True))
+print (type (1.5 *2))
+print (type (False))
+print (type ("texto") )
+
+ONE, two = 10.30, 22
+ONE = (ONE)
+
+
+print(ONE, two)
+
+nome = input('Whats your name?')
+idade = input("How old are you?")
+
+
+# print (nome, idade)
+# print (nome, idade, end="...\n")
+# print (nome, idade, sep="#", end=".......\n")
+# print (nome, idade, sep="#")
+
+print(float("a"))
