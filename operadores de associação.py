6bca9e57553776d85d4d992337bc4a85f7ad0484
Virgílio César <enjels@outlook.com>
2024-04-21 11:38:42 -0300

    exemplose de operadores de verificação

---
+++ operadores de associação.py
@@ -0,0 +1,10 @@
+curso = "Curso de Python"
+
+frutas = ["laranja", "uva", "limão"]
+
+saques = [1500, 100]
+# "in" usado para verificar se algo esta presente na frase do exemplo "
+print("Python" in curso)
+# "not in" usado para verificar se algo não esta presente na lsita de frutas do exemplo "
+print("maçã" not in frutas)
+print(200 in saques)
